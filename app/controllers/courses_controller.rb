32b2f6593b10e4a1303ecb1960232e23247bf801
Oskar Emilsson <oskaremilsson@gmail.com>
2018-09-30 11:22:39 +0200

    Better errors

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,28 +3,37 @@ class CoursesController < ApplicationController
     before_action :is_admin?, only: [:new, :create, :edit]
 
     def index
-        if valid_filter?
-            courses = Course.where(params[:filter] => params[:on])
+        errors = []
+        if params[:filter] && params[:on]
+            if valid_filter?
+                courses = Course.where(params[:filter] => params[:on])
+                errors.push('no courses found') unless courses.length > 0
+            else
+                errors.push('filter not valid')
+            end
         elsif
             courses = Course.all
+            errors.push('no courses found') unless courses.length > 0
         end
 
         data = {
-            nrOfCourses: courses.length,
             filtered: valid_filter?,
             courses: courses,
-            :errors => ({ error: 'no courses found' } unless courses.length > 0)
+            errors: errors
         }.compact
         
         render :json => data
     end
 
     def show
+        errors = []
         course = Course.find_by(id: params[:id])
+
+        errors.push('course not found') unless course
 
         data = {
             course: course,
-            :errors => ({ error: 'course not found' } unless course)
+            errors: errors
         }.compact
 
         render :json => data
@@ -66,13 +75,8 @@ class CoursesController < ApplicationController
         end
 
         def valid_filter?
-            false
-            if params[:filter] && params[:on]
-                if Course.column_names.include? params[:filter]
-                    true
-                else
-                    false
-                end
+            if Course.column_names.include? params[:filter]
+                true
             else
                 false
             end
